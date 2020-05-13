9c00eaf0d94d80ca40b228ce9f3c40526d3a7af1
Adrian Pop <pop.adrian61@gmail.com>
2020-05-13 18:36:00 +0300

    Build package with cmake instead of Makefile. Now the test package can be opened with QtCreator, same code runs on desktop (SDL is available both on desktop and Embedded platforms).

--- package/hello/hello.mk
+++ package/hello/hello.mk
@@ -7,7 +7,7 @@
 HELLO_VERSION = 1.0
 HELLO_SITE = ../STM32F769I-disco_Buildroot/package/hello/src
 HELLO_SITE_METHOD = local
-HELLO_DEPENDENCIES = sdl
+HELLO_DEPENDENCIES = sdl sdl_ttf freetype zlib
 
 define HELLO_BUILD_CMDS
     $(MAKE) $(TARGET_CONFIGURE_OPTS) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D)
@@ -17,4 +17,4 @@ define HELLO_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/hello $(TARGET_DIR)/usr/bin
 endef
 
-$(eval $(generic-package))
+$(eval $(cmake-package))
